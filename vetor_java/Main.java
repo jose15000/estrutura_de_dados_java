b4bc0ddf4627d1ac66b4041e006e8ab7385720fa
José Henrique Oliveira de Carvalho <98109872+jose15000@users.noreply.github.com>
2023-02-28 01:14:10 -0300

    minor changes on main

--- vetor_java/Main.java
+++ vetor_java/Main.java
@@ -7,6 +7,8 @@ public class Main{
         Aluno a1 = new Aluno();
         Aluno a2 = new Aluno();
 
+        aluno.Add(a1, 0);
+
         
 
         System.out.println("Aluno:" + aluno.recuperar(0).getNome() + "\n" + "Existe dado: " + aluno.existeDado(0) + "\n" + "Tamanho do vetor: " + aluno.Length() + "\n" 
